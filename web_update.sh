2cf0712779f602d22bac0458e5b4c935ff99b60b
V-Z <vojta@trapa.cz>
2018-01-05 16:36:59 +0100

    Mainly web cleanup.

---
+++ web_update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# After every Git commit affecting web...
+# Use './web_update.sh all' to update also presentation and scripts and data
+
+# The server
+REMOTE=`grep lad ~/.bashrc | grep -o "[a-z0-9.]\+@[a-z0-9.]\+"`
+
+echo "Syncing 'rcourse' directory"
+echo
+rsync -arv --exclude=*~ --progress rcourse $REMOTE:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
+echo
+
+# Copy also presentation and scripts and data
+if [ "$1" == "all" ]; then
+	# Copy presentation
+	echo "Syncing presentation"
+	echo
+	rsync -av --progress presentation/r_mol_data_phylogen.pdf $REMOTE:~/soubory.trapa.cz/htdocs/rcourse/ || { echo "Error!" && exit 1; }
+	echo
+	# Data
+	# csv, html, in, nexus, nwk, r, txt
+	fi
+
+echo "Done!"
+
+exit
