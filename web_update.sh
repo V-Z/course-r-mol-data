74571db7cfaedefdd95ed464a1ffde6193cfc212
V-Z <vojta@trapa.cz>
2020-01-23 12:25:24 +0100

    Updates for 2020, changes in some packages, changed aspect ratio to 16:9.
    Signed-off-by: V-Z <vojta@trapa.cz>

--- web_update.sh
+++ web_update.sh
@@ -3,12 +3,9 @@
 # After every Git commit affecting web...
 # Use './web_update.sh all' to update also presentation and scripts and data
 
-# The server
-REMOTE=`grep laddobar ~/.bashrc | grep -o "[a-z0-9.]\+@[a-z0-9.]\+"`
-
 echo "Syncing 'rcourse' directory"
 echo
-rsync -arv --exclude=*~ --progress rcourse $REMOTE:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
+rsync -arv --exclude=*~ --progress rcourse laddobar:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
 echo
 
 # Copy also presentation and scripts and data
@@ -16,7 +13,7 @@ if [ "$1" == "all" ]; then
 	# Copy presentation
 	echo "Syncing presentation"
 	echo
-	rsync -av --progress presentation/r_mol_data_phylogen.pdf $REMOTE:~/soubory.trapa.cz/htdocs/rcourse/ || { echo "Error!" && exit 1; }
+	rsync -av --progress presentation/r_mol_data_phylogen.pdf laddobar:~/soubory.trapa.cz/htdocs/rcourse/ || { echo "Error!" && exit 1; }
 	echo
 	# Data
 	# csv, html, in, nexus, nwk, r, txt
