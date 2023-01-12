fada0e927709b2adc3cc6a4dd1c96b9cbd08b8b2
Vojtěch Zeisek <vojta@trapa.cz>
2023-01-12 11:03:42 +0100

    Updates dates for 2023.

--- web_update.sh
+++ web_update.sh
@@ -5,7 +5,7 @@
 
 echo "Syncing 'rcourse' directory"
 echo
-rsync -arv --exclude=*~ --progress rcourse laddobar:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
+rsync -arv --exclude=*~ --progress rcourse striga:~/soubory.trapa.cz/htdocs/ || { echo "Error!" && exit 1; }
 echo
 
 # Copy also presentation and scripts and data
@@ -13,7 +13,7 @@ if [ "$1" == "all" ]; then
 	# Copy presentation
 	echo "Syncing presentation"
 	echo
-	rsync -av --progress presentation/r_mol_data_phylogen.pdf laddobar:~/soubory.trapa.cz/htdocs/rcourse/ || { echo "Error!" && exit 1; }
+	rsync -av --progress presentation/r_mol_data_phylogen.pdf striga:~/soubory.trapa.cz/htdocs/rcourse/ || { echo "Error!" && exit 1; }
 	echo
 	# Data
 	# csv, html, in, nexus, nwk, r, txt
